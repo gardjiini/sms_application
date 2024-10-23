4d4abb7d27e34b286559080b2fc1c2f1a850e7cc
gardjiini <okyeremarfo66@gmail.com>
2024-10-23 08:26:31 +0000

    Updated project structure and code

--- sms_project/sms_app/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class SMSSendForm(forms.Form):
-    to = forms.CharField(label='Phone Number', max_length=15)
-    message = forms.CharField(widget=forms.Textarea, label='Message')
